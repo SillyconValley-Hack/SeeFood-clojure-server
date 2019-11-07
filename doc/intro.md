33b5db318957a7b39766c04d9babd96392d0e487
giulianchiu <julian@pro-duction.com.tw>
2019-11-07 20:01:28 +0800

    Google vision api clean app template

---
+++ doc/intro.md
@@ -0,0 +1,3 @@
+# Introduction to googlevision
+
+TODO: write [great documentation](http://jacobian.org/writing/what-to-write/)
---
+++ src/googlevision/core.clj
@@ -0,0 +1,7 @@
+(ns googlevision.core
+  (:gen-class))
+
+(defn -main
+  "I don't do a whole lot ... yet."
+  [& args]
+  (println "Hello, World!"))
