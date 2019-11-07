207ee72234ed2a58b2a96bfb142882bb5e4ca3f8
JerryShen <jerry82422@gmail.com>
2019-11-07 21:47:17 +0800

    Finish google vision request and response

--- project.clj
+++ project.clj
@@ -4,7 +4,8 @@
   :license {:name "EPL-2.0 OR GPL-2.0-or-later WITH Classpath-exception-2.0"
             :url "https://www.eclipse.org/legal/epl-2.0/"}
   :dependencies [[org.clojure/clojure "1.10.0"]
-                 [com.google.cloud/google-cloud-vision "1.25.0"]
+                 [com.google.cloud/google-cloud-vision "1.75.0"]
+                 [environ "1.1.0"]
                  ]
   :main ^:skip-aot googlevision.core
   :target-path "target/%s"
--- src/googlevision/core.clj
+++ src/googlevision/core.clj
@@ -1,6 +1,16 @@
 (ns googlevision.core
   (:require [clojure.java.io :as io]
-            )
+            [environ.core :refer [env]]
+  )
+  (:import [com.google.cloud.vision.v1 ImageAnnotatorClient]
+           [com.google.cloud.vision.v1 Feature]
+           [com.google.cloud.vision.v1 Image]
+           [com.google.cloud.vision.v1 AnnotateImageRequest]
+           [com.google.cloud.vision.v1 AnnotateImageResponse]
+           [com.google.cloud.vision.v1 BatchAnnotateImagesResponse]
+           [com.google.protobuf ByteString]
+           [java.io FileInputStream]
+  )
 
   (:gen-class))
 
@@ -8,7 +18,20 @@
   "I don't do a whole lot ... yet."
   [& args]
   ;
-  (println (.exists (io/file "./public/Hot-Dogs.jpg" ))
-           )
+  ; (println (.exists (io/file "./public/Hot-Dogs.jpg" ))
+  ; (println (env :GOOGLE_APPLICATION_CREDENTIALS)
+  (let [client (ImageAnnotatorClient/create)
+        photo-in-byte (ByteString/readFrom (new FileInputStream "./public/Hot-Dogs.jpg" ))
+        image (.build (.setContent (Image/newBuilder) photo-in-byte))
+        feature (.build (.setType (Feature/newBuilder) com.google.cloud.vision.v1.Feature$Type/LABEL_DETECTION))
+        request (.addFeatures (AnnotateImageRequest/newBuilder) feature)
+        request (.setImage request image)
+        request (.build request)
+
+        response (.batchAnnotateImages client [request])
+        response (.getResponsesList response)
+       ]
+    (println response)
+  )
 )
 
